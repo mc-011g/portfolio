a81a20ab80aade47ce4f0c4b6a02c74a2691ab06
Matthew Carmichael <mcarmichael011@gmail.com>
2026-05-31 17:45:50 -0600

    Updated not found page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -3,8 +3,8 @@ import { Container } from "../components/Container";
 const NotFound = () => {
 
     return (
-        <section aria-label='privacy-notice' className='scroll-mt-[112px] sm:scroll-mt-[96px] mt-[112px] sm:mt-[96px] h-full w-full' id='privacy-notice'>
-            <Container>
+        <section aria-label='privacy-notice' className='scroll-mt-[112px] sm:scroll-mt-[96px] mt-[112px] sm:mt-[96px] w-full dark:bg-gray-800 h-[calc(100vh-96px)]' id='privacy-notice'>
+            <Container dark={false}>
                 <div className="flex flex-col items-center justify-center gap-4 text-center">
                     <h1 className='text-[36px]/[36px] md:text-[48px]/[48px] lg:text-[60px]/[60px] font-bold w-full'>Page Not Found</h1>
 
